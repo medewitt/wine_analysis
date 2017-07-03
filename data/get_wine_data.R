f6a9746ef3ab5704b149ca4796bc78628e0b3611
medewitt <me.dewitt.jr@gmail.com>
2017-07-03 17:07:03 -0400

    Updated from read_delim to read.csv. Was dropping some values as NA. Fixed now.

--- data/get_wine_data.R
+++ data/get_wine_data.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 
-red_wine_data <- read_delim("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",
-                          delim = ";")
+red_wine_data <-as_tibble(read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",
+                          sep =  ";"))
 
 colnames(red_wine_data)<-c("fixed_acidity", "volatile_acidity", "citric_acid",
                            "residual_sugar", "chlorides", "free_sulfur_dioxide",
                            "total_sulfur_dioxide", "density", "pH", "sulphates",
                            "alcohol", "quality")
 red_wine_data <- mutate(red_wine_data, total_sulfur_dioxide = as.numeric(total_sulfur_dioxide))
+
+summary(red_wine_data)
