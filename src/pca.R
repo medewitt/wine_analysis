79fcb24dc9ee99cb0e85b12b84b77b0a0076e1de
medewitt <me.dewitt.jr@gmail.com>
2017-07-03 17:04:49 -0400

    principal components analysis to seek out the most influential parameters. Can be used with SVM if we go that route.

---
+++ src/pca.R
@@ -0,0 +1,14 @@
+#Purpose: Run principle component analysis and 
+#         see if we can reduce the dimensionality in the dataset
+
+pca_red_wine<- red_wine_data %>%
+  select(-quality) %>% 
+  as.data.frame() %>% 
+  na.omit()
+
+summary(pca_red_wine)
+str(pca_red_wine)
+
+(pca<-prcomp(pca_red_wine, scale. = TRUE))
+summary(pca)
+plot(pca)
