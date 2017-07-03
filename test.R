4dd0317fc80a11b6621464889295c455213e94cf
medewitt <me.dewitt.jr@gmail.com>
2017-07-03 15:04:28 -0400

    Add a purpose line and error check to ensure all have installed project template for easy loading of data and scripts.

--- test.R
+++ test.R
@@ -1,2 +1,9 @@
-library(ProjectTemplate)
+#Purpose: The purpose of this file is to load the project template library and load the project.
+
+if(!require(ProjectTemplate)){
+  install.packages("somepackage")
+  library(ProjectTemplate)
+}
+  
 load.project()
+
