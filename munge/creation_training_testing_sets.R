5ede9497d0d258b79933541237933e44f7588204
medewitt <me.dewitt.jr@gmail.com>
2017-07-03 22:32:49 -0400

    delete dumplicates

--- munge/creation_training_testing_sets.R
+++
@@ -1,9 +0,0 @@
-#The purpose of this script is to segregate the data into testing and training data sets
-
-set.seed(336)
-
-n <- nrow(red_wine_data_factors)
-train_rows <- sample(seq(n), size = .7 * n)
-
-red_wine_data_training <- red_wine_data_factors[ train_rows, ]
-red_wine_data_testing  <- red_wine_data_factors[-train_rows, ]
