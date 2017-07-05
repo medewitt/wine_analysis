4cdf73747ba885c106168d2d362abbab97e03a45
medewitt <me.dewitt.jr@gmail.com>
2017-07-05 11:25:15 -0400

    added feature selection using subset and k folds cross validation

--- src/linear_model.R
+++ src/linear_model.R
@@ -1,5 +1,7 @@
 #Purpose: multi-linear regression
 library(boot)
+library(leaps)
+set.seed(336)
 #Create the full linear model
 (full_lm_model <- lm(as.numeric(red_wine_data_training$quality)~., data = red_wine_data_training))
 
@@ -50,3 +52,33 @@ boot_lm <- function (data, index){
 boot_lm(red_wine_data, sample(100, 100, replace = T))
 
 boot(red_wine_data, boot_lm, 1000)
+
+#Feature selection using model selection and cv
+
+predict.regsubsets <- function (object, newdata, id, ...){
+  form = as.formula (object$call[[2]])
+  mat = model.matrix(form, newdata)
+  coefi = coef(object, id= id)
+  xvars = names (coefi)
+  mat[,xvars]%*%coefi
+}
+
+k <- 10
+folds <- sample (1:k, nrow(red_wine_data), replace = TRUE)
+cv_error <- matrix(NA, k, 11, dimnames = list(NULL, paste(1:11)))
+
+for (j in 1:k){
+  best_fit <- regsubsets(quality~., data = red_wine_data [folds !=j,],
+                         nvmax = 11)
+  for (i in 1:11){
+    pred = predict.regsubsets(best_fit, red_wine_data[folds==j,], id = i)
+    cv_error[j,i] = mean( (red_wine_data$quality[folds==j]-pred)^2)
+  }
+}
+
+(mean_cv_errors <- apply(cv_error, 2, mean))
+plot(mean_cv_errors, type = 'b')
+which.min(mean_cv_errors)
+
+reg_best <- regsubsets(quality ~., data = red_wine_data, nvmax = 7)
+coef(reg_best, 7)
