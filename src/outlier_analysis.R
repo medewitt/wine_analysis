9c117118cec86b05fe6a2a51de048d50e8106a95
medewitt <me.dewitt.jr@gmail.com>
2017-07-21 16:22:57 -0400

    added more plots to look for outliers

--- src/outlier_analysis.R
+++ src/outlier_analysis.R
@@ -14,3 +14,19 @@ plot(fit3, main = "Residual Sugar")
 fit4 <-lm(quality ~ chlorides, data = red_wine_data)
 par(mfrow=c(2,2))
 plot(fit3, main = "Chlorides")
+
+fit5 <-lm(quality ~ alcohol, data = red_wine_data)
+par(mfrow=c(2,2))
+plot(fit5, main = "Alcohol")
+
+fit6 <-lm(quality ~ volatile_acidity, data = red_wine_data)
+par(mfrow=c(2,2))
+plot(fit6, main = "Alcohol")
+
+fit7 <-lm(quality ~ pH, data = red_wine_data)
+par(mfrow=c(2,2))
+plot(fit7, main = "pH")
+
+fit8 <-lm(quality ~ citric_acid, data = red_wine_data)
+par(mfrow=c(2,2))
+plot(fit8, main = "Citric Acid")
