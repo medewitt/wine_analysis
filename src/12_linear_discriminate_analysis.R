cc964592a4d625b522f332355f7c9b7e2fc66228
medewitt <me.dewitt.jr@gmail.com>
2017-07-23 17:39:20 -0400

    removed quality numerical value which was resultign in leakage in the analsysis and showing artifically higher values

--- src/12_linear_discriminate_analysis.R
+++ src/12_linear_discriminate_analysis.R
@@ -2,19 +2,22 @@
 
 library(MASS)
 
+red_wine_data_training_fact <- dplyr::select(red_wine_data_training, -quality)
+red_wine_data_testing_fact <- dplyr::select(red_wine_data_testing, -quality)
+
 #create the LDA Model
-lda_model <- lda(formula = classification ~., data = red_wine_data_training, 
+lda_model <- lda(formula = classification ~., data = red_wine_data_training_fact, 
                  prior = c(1,1,1)/3, CV = FALSE)
 
 #Use the LDA model against the testing data set
 
-pred_lda_model <- predict(lda_model, newdata = red_wine_data_testing)
+pred_lda_model <- predict(lda_model, newdata = red_wine_data_testing_fact)
 
 head(pred_lda_model$posterior,3)
 
 #Look at the confusion matrix
 
-table(pred_lda_model$class, red_wine_data_testing$classification)
+table(pred_lda_model$class, red_wine_data_testing_fact$classification)
 #-----------------------------------------------------------------------------------------
 #Tune
 table(red_wine_data_factors$classification)
@@ -23,15 +26,16 @@ table(red_wine_data_factors$classification)
 63/1599
 
 #create the LDA Model
-lda_model <- lda(formula = classification ~., data = red_wine_data_training, 
-                 prior = c(.1357, 0.8249, 0.0394), CV = FALSE)
+lda_model <- lda(formula = classification ~., data = red_wine_data_training_fact, CV = FALSE)
 
 #Use the LDA model against the testing data set
 
-pred_lda_model <- predict(lda_model, newdata = red_wine_data_testing)
+pred_lda_model <- predict(lda_model, newdata = red_wine_data_testing_fact)
 
 head(pred_lda_model$posterior,3)
 
 #Look at the confusion matrix
 
-(lda_table <-table(pred_lda_model$class, red_wine_data_testing$classification))
+(lda_table <-table(pred_lda_model$class, red_wine_data_testing_fact$classification))
+
+(lda_missclass<-mean(pred_lda_model$class!=red_wine_data_testing_fact$classification))
