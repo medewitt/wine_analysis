37088408978a35d85c6e9368236e688b4c167f6c
medewitt <me.dewitt.jr@gmail.com>
2017-07-25 20:42:01 -0400

    added outlier removal function that looks at 1.5x the IQR. We need to decide if we use it or not

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,3 +1,12 @@
 range_scaler <- function(x){
   x = x- min(x)/(max(x)-min(x))
 }
+
+remove_outliers <- function(x, na.rm = TRUE, ...) {
+  qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+  H <- 1.5 * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+}
