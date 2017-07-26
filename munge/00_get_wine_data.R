28b1c6569d7532a094fef30fc5f2fda0319e3116
medewitt <me.dewitt.jr@gmail.com>
2017-07-25 20:42:54 -0400

    Made two separate df for the data for factors and for regression. _fact represents factors only

--- munge/00_get_wine_data.R
+++ munge/00_get_wine_data.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 
-red_wine_data <-as_tibble(read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",
+red_wine_data_raw <-as_tibble(read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",
                           sep =  ";"))
 
-colnames(red_wine_data)<-c("fixed_acidity", "volatile_acidity", "citric_acid",
+colnames(red_wine_data_raw)<-c("fixed_acidity", "volatile_acidity", "citric_acid",
                            "residual_sugar", "chlorides", "free_sulfur_dioxide",
                            "total_sulfur_dioxide", "density", "pH", "sulphates",
                            "alcohol", "quality")
-red_wine_data <- mutate(red_wine_data, total_sulfur_dioxide = as.numeric(total_sulfur_dioxide))
+red_wine_data_raw <- mutate(red_wine_data_raw, total_sulfur_dioxide = as.numeric(total_sulfur_dioxide))
 
-summary(red_wine_data)
+
+
--- munge/01_add_factors_red_wine.R
+++ munge/01_add_factors_red_wine.R
@@ -6,10 +6,13 @@ good_threshold <- 7
 
 #This script creates the new levels and factors in a new data set
 
-red_wine_data_factors<- red_wine_data %>% 
+red_wine_data_factors<- red_wine_data_raw %>% 
   mutate(classification = case_when (quality <= poor_threshold ~ "poor", 
                                   quality >= good_threshold ~ "good", 
                                   TRUE ~"medium"))
+red_wine_data_factors <- red_wine_data_factors %>% 
+  dplyr::select(-quality)
+
 table(red_wine_data_factors$classification)
 
 red_wine_data_factors <- red_wine_data_factors %>% 
