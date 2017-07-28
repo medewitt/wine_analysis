fa9c575f2422738092c396162292c7e64c3f6359
Michael Dewitt-Jr <N540723@adgroup.michelin.com>
2017-07-28 13:40:26 -0400

    Added boosting methods to our analysis

---
+++ src/05_boosted_reg.R
@@ -0,0 +1,17 @@
+library(gbm)
+
+#Perform boosting regression analysis
+
+boost_wine <- gbm(quality ~., data = red_wine_data_training, distribution = "gaussian", n.trees = 5000,
+                  interaction.depth = 4)
+
+
+#partial dependence plots
+
+par(mfrow = c(2,2))
+plot(boost_wine, i = "alcohol")
+plot(boost_wine, i = "citric_acid")
+
+yhat_boost <- predict(boost_wine, newdata = red_wine_data_testing, n.trees = 5000)
+
+(MSE_boost_reg <- mean((yhat_boost-red_wine_data_testing$quality)^2))
