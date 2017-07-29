929fda8552013e09868adbf32e0a8b7e98178c83
medewitt <me.dewitt.jr@gmail.com>
2017-07-28 21:41:32 -0400

    Commented out the code. I forgot that this is a multi-class so the bernouilli dist doesn't work. Doing some research we would need to go a an adaBoost also developed by HAstie. Not going to go into that for this class.

--- src/16_boost_classification.R
+++ src/16_boost_classification.R
@@ -1,18 +1,18 @@
-library(gbm)
-
-#Perform boosting regression analysis
-
-boost_wine_fact <- gbm(classification ~., data = red_wine_data_training_fact, distribution = "bernoulli", n.trees = 5000,
-                  interaction.depth = 4)
-
-
-#partial dependence plots
-
-par(mfrow = c(2,2))
-plot(boost_wine_fact, i = "alcohol")
-plot(boost_wine_fact, i = "citric_acid")
-
-yhat_boost <- predict(boost_wine_fact, newdata = red_wine_data_testing_fact, n.trees = 5000)
-
-(boost_missclassification <- 1-
-    mean(yhat_boost== red_wine_data_testing_fact$classification))
+# library(gbm)
+# 
+# #Perform boosting regression analysis
+# 
+# boost_wine_fact <- gbm(classification ~., data = red_wine_data_training_fact, distribution = "bernoulli", n.trees = 5000,
+#                   interaction.depth = 4)
+# 
+# 
+# #partial dependence plots
+# 
+# par(mfrow = c(2,2))
+# plot(boost_wine_fact, i = "alcohol")
+# plot(boost_wine_fact, i = "citric_acid")
+# 
+# yhat_boost <- predict(boost_wine_fact, newdata = red_wine_data_testing_fact, n.trees = 5000)
+# 
+# (boost_missclassification <- 1-
+#     mean(yhat_boost== red_wine_data_testing_fact$classification))
