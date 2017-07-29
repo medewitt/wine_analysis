e5b17ac66dcbc91142d8490109ab7cc964da1b3e
medewitt <me.dewitt.jr@gmail.com>
2017-07-28 21:42:06 -0400

    Tress classification added. I had to convert the prediction values to actual categorical values. Returned terrible results for error.

--- src/17_tree_classification.R
+++ src/17_tree_classification.R
@@ -3,6 +3,7 @@ library(tree)
 
 tree_wine <- tree(classification ~., data = red_wine_data_training_fact)
 
+par(mfrow=c(1,1))
 plot(tree_wine)
 text(tree_wine, pretty = 0)
 
@@ -10,9 +11,16 @@ cv_tree_wine <- cv.tree( tree_wine, FUN = prune.misclass)
 
 plot(cv_tree_wine$size, cv_tree_wine$dev, type = "b")
 
+#Choose 6 branchs based on the chart
+
 prune_wine_tree <- prune.misclass(tree_wine, best = 6)
 
 wine_tree_pred <-predict(prune_wine_tree, newdata = red_wine_data_testing_fact)
 
+wine_preds <- colnames(wine_tree_pred)[apply(wine_tree_pred,1,which.max)]
+
 (tree_missclassification <- 1-
-    mean(wine_tree_pred== red_wine_data_testing_fact$classification))
+    mean(wine_preds== red_wine_data_testing_fact$classification))
+
+table(wine_preds, red_wine_data_testing_fact$classification)
+
