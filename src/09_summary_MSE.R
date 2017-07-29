8d465f2ef44477d868b1181aeff11fd178639b5f
medewitt <me.dewitt.jr@gmail.com>
2017-07-29 07:52:22 -0400

    changed number so this script is ran before the agregation of values

---
+++ src/09_summary_MSE.R
@@ -0,0 +1,18 @@
+#Now Combine all the Test MSE to a single files
+
+
+combined_linear_mse <- as_tibble(rbind(MSE_lsr, MSE_ridge, MSE_lasso, MSE_pcr, MSE_pls, MSE_boost_reg)) %>% 
+  mutate(Fit_Technique = c("Least Squares", "Ridge", "Lasso", "PCR", "PLS", "Boosted Regression"))
+
+colnames(combined_linear_mse)<-c("MSE", "Model")
+
+combined_linear_mse<- combined_linear_mse %>% 
+  mutate(MSE_round = round (MSE, 3))
+
+(MSE_plot <-ggplot(combined_linear_mse, aes (x= Model, y = MSE))+
+  geom_bar(stat = "identity")+
+  labs(
+    title = "Overall MSE for Each Regression Type",
+    subtitle = "Predicting the Quality Asssement"
+  )+
+  geom_label(aes(x= Model, y = MSE, label = MSE_round ), position = "dodge"))
