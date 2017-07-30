523a755d18af73a6dd0aa2c8fcfff91f0a5ce91a
medewitt <me.dewitt.jr@gmail.com>
2017-07-30 11:56:12 -0400

    generatedrelative imp plot for final rep

--- src/05_boosted_reg.R
+++ src/05_boosted_reg.R
@@ -5,6 +5,16 @@ library(gbm)
 boost_wine <- gbm(quality ~., data = red_wine_data_training, distribution = "gaussian", n.trees = 5000,
                   interaction.depth = 4)
 
+par(mfrow=c(1,1))
+summary(boost_wine)
+
+boost_reg_outputs <- summary(boost_wine)
+
+(boost_reg_plot <- ggplot(boost_reg_outputs, aes(x= reorder(var, rel.inf), y = rel.inf))+
+  geom_bar(stat = "identity")+
+    coord_flip()+
+    ylab("Relative Importantance")+
+    xlab("Predictor"))
 
 #partial dependence plots
 
