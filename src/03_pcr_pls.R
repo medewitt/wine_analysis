2ed303fd424cfee246c42b241cfc46e7a0d3c999
medewitt <me.dewitt.jr@gmail.com>
2017-07-30 17:41:38 -0400

    Added cpde for screeplots

--- src/03_pcr_pls.R
+++ src/03_pcr_pls.R
@@ -12,11 +12,18 @@ pcr_fit$loadings
 PCR_pred <- predict( pcr_fit, red_wine_data_testing, ncomp=9 )
 (MSE_pcr = mean( ( red_wine_data_testing$quality - PCR_pred )^2 ))
 
+(pcr_x<-pcr_fit$Xvar/sum(pcr_fit$Xvar))
+
+plot(cumsum(pcr_x), xlab = "Number of Components", ylab = "Cumulative Porportion of Variance Explainned")
 
 #PLS Regression
 
 pls_fit <- plsr( quality ~ ., data = red_wine_data_clean, subset = train_rows, 
                  scale=TRUE, validation="CV" )
+
+(pls_x<-pls_fit$Xvar/sum(pls_fit$Xvar))
+
+plot(cumsum(pls_x), xlab = "Number of Components", ylab = "Cumulative Porportion of Variance Explainned")
 
 summary(pls_fit)
 
