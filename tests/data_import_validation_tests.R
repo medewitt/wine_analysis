30a2e06677aba94da68f4997b9293fefe7e53d65
medewitt <me.dewitt.jr@gmail.com>
2017-07-31 06:34:54 -0400

    last knit with final edits from David

--- tests/data_import_validation_tests.R
+++ tests/data_import_validation_tests.R
@@ -3,12 +3,12 @@
 library(validate)
 expect_that(1, equals(1))
 
-(cf <-red_wine_data %>% 
+(cf <-red_wine_data_raw %>% 
   check_that(fixed_acidity >0, volatile_acidity>0, quality >0, residual_sugar>0, chlorides>0, 
-             is.na(red_wine_data)==FALSE, nrow(red_wine_data)==1599, ncol(red_wine_data)==12))
+             is.na(red_wine_data_raw)==FALSE, nrow(red_wine_data_raw)==1599, ncol(red_wine_data_raw)==12))
 
 summary(cf)
-names(red_wine_data)
+names(red_wine_data_raw)
 par(mfrow = c(1,1))
 barplot(cf, main="data import tests")
 
